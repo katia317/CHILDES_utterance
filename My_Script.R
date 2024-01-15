2e6cc3d98cb00159b35ee17238fc35a3bd4197e4
羊咔嚓 <katia@yangkachadeMacBook-Pro.local>
2024-01-14 23:52:37 -0600

    completed Assignment 4

--- My_Script.R
+++ My_Script.R
@@ -3,12 +3,29 @@ library(tidyverse)
 library(papaja)
 library(ggplot2)
 
-# Assign 1+ numeric variable and 1+ string/character variable
+# Assignment 1
 num_v <- 22
 str_v <- "Jimmy Butler"
 
-# BONUS: Create a dataframe
 Miami_Heat <- data.frame(
   Players = c("Butler", "Adebayo", "Herro", "Robinson", "Love"),
   Numbers = c(22, 13, 14, 55, 42)
 )
+
+# Assignment 4
+# prints a greeting based on "name"
+hello_world <- function(name) {
+  if (name == "Dr.Dowling"){
+    paste0("Good morning", "," , name, "!")
+  }
+  else if (name == "Voldemort") {
+    paste0(name, ", Expelliarmus!")
+  }
+  else if (name %in% c("Monica","Phoebe","Rachel")) {
+    paste0(name, ", How you doing?")
+  }
+  else {
+    paste0("I don’t know you:(")
+  }
+}
+
